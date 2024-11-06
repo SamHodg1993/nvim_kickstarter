3cf5fef326fdb2cde94a206a8c9330525ce75bbe
sam <samhodgkinson9192@gmail.com>
2024-11-06 20:31:00 +0000

    Installed a go lsp and edited the harpoon config file to replace a depricated function call

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -12,7 +12,7 @@ return {
 
     -- Control and view harpoon
     vim.keymap.set('n', '<leader>a', function()
-      harpoon:list():append()
+      harpoon:list():add()
     end)
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
